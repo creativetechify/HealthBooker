50a199ce4075fc382aed53bd37376cd3b08fd12e
Creative Techify <learnwithfun4567@gmail.com>
2023-07-10 15:06:53 +0530

    commit777

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,15 +8,14 @@ const Hero = () => {
       <div className="hero-content">
         <h1>
           Spiritual Healing <br />
-          by Dr.Dhanesh
+          by Dr.Dhanesh Kumar Muchal
         </h1>
         <p>
-          Welcome to our website! At Spiritual Healing by Dhanesh , we are
-          dedicated to providing you with the highest quality holistic
-          healthcare options. One of the effective modalities we offer is
-          Craniosacral Therapy (CST). In this article, we will delve into the
-          intricacies of CST, exploring its principles, benefits, and how it can
-          enhance your overall well-being.
+          Welcome to our website! , we are dedicated to providing you with the
+          highest quality holistic healthcare options. One of the effective
+          modalities we offer is Craniosacral Therapy (CST). In this article, we
+          will delve into the intricacies of CST, exploring its principles,
+          benefits, and how it can enhance your overall well-being.
         </p>
       </div>
       <div className="hero-img">
